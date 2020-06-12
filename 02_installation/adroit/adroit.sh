97b3ca4cbe53f1cf140a6ff2b8f49da31242dec0
jdh4 <52128661+jdh4@users.noreply.github.com>
2020-06-12 09:47:32 -0400

    Update adroit.sh

--- 02_installation/adroit/adroit.sh
+++ 02_installation/adroit/adroit.sh
@@ -30,28 +30,3 @@ cmake3 .. -DCMAKE_BUILD_TYPE=Release \
 make -j 10
 make check
 make install
-
-#############################################################
-# build mdrun_mpi (for multi-node MPI jobs)
-#############################################################
-
-cd ..
-mkdir build_stage2
-cd build_stage2
-
-module load intel-mpi/intel/2018.3/64
-
-cmake3 .. -DCMAKE_BUILD_TYPE=Release \
--DCMAKE_C_COMPILER=icc -DCMAKE_C_FLAGS_RELEASE="$OPTFLAGS" \
--DCMAKE_CXX_COMPILER=icpc -DCMAKE_CXX_FLAGS_RELEASE="$OPTFLAGS" \
--DGMX_BUILD_MDRUN_ONLY=ON -DGMX_MPI=ON -DGMX_OPENMP=ON \
--DGMX_SIMD=AVX_512 -DGMX_DOUBLE=OFF \
--DGMX_FFT_LIBRARY=mkl \
--DGMX_GPU=ON -DGMX_CUDA_TARGET_SM=70 \
--DCMAKE_INSTALL_PREFIX=$HOME/.local \
--DGMX_COOL_QUOTES=OFF -DREGRESSIONTEST_DOWNLOAD=ON
-
-make -j 10
-source ../build_stage1/scripts/GMXRC
-tests/regressiontests-${version}/gmxtest.pl all
-make install
